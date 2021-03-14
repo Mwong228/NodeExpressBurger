18a31c859caa08113f8768db5254eb3dc21c0224
Michael Wong <michael.w228@gmail.com>
2021-03-13 19:48:04 -0500

    added server connection

--- server.js
+++ server.js
@@ -1,3 +1,17 @@
 const express = require('express')
-const mysql = require('mysql')
+const app = express()
 const db = require( './app/connection' )('burgers','pass1234')
+const exphbs = require('express-handlebars')
+
+
+const PORT = process.env.PORT || 3000 
+
+app.use(express.static('pulic'))
+app.use(express.urlencoded({extended:true}))
+app.use(express.json())
+
+
+
+app.listen(PORT, function(){
+    console.log(`Serving burgers on PORT ${PORT}`)
+})
